f01e08b2c6f00350aba6e65695eb3c83d8d52e23
juancito.eth <juancitoeth@gmail.com>
2022-11-10 14:39:35 -0300

    add user feedback links
    #solves 40

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,20 +3,39 @@ import styles from "../styles/Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <span role="img" aria-label="sheep">
-        Built with 🌿 by
-      </span>
-      <a href="https://lenster.xyz/u/juancito.lens" target="blank">
-        juancito.lens
-      </a>
-      &nbsp; | &nbsp;
-      <a
-        href="https://github.com/0xJuancito/inlens"
-        target={"_blank"}
-        rel={"noreferrer"}
-      >
-        GitHub
-      </a>
+      <div>
+        <a
+          href="https://inlens.canny.io/feature-requests"
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          Feedback
+        </a>
+        &nbsp; | &nbsp;
+        <a
+          href="https://inlens.canny.io/bugs"
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          Report a Bug
+        </a>
+        &nbsp; | &nbsp;
+        <a
+          href="https://github.com/0xJuancito/inlens"
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          GitHub
+        </a>
+      </div>
+      <div>
+        <span role="img" aria-label="sheep">
+          Built with 🌿 by
+        </span>
+        <a href="https://lenster.xyz/u/juancito.lens" target="blank">
+          juancito.lens
+        </a>
+      </div>
     </footer>
   );
 };
